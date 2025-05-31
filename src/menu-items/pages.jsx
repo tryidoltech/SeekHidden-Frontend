42eae7fdbb5a8936492719134ce2c54014cfdd74
haquedot <haquedot@gmail.com>
2025-05-31 15:33:59 +0530

    setup sidebar

--- src/menu-items/pages.jsx
+++ src/menu-items/pages.jsx
@@ -18,56 +18,19 @@ const pages = {
   type: 'group',
   children: [
     {
-      id: 'maintenance',
-      title: 'maintenance',
+      id: 'others',
+      title: 'others',
       type: 'collapse',
       icon: icons.maintenance,
       children: [
         {
-          id: 'error-404',
-          title: 'error-404',
+          id: 'clients',
+          title: 'Clients',
           type: 'item',
-          url: '/maintenance/404',
-          target: true
+          url: '/dashboard/clients',
         },
-        {
-          id: 'error-500',
-          title: 'error-500',
-          type: 'item',
-          url: '/maintenance/500',
-          target: true
-        },
-        {
-          id: 'coming-soon',
-          title: 'coming-soon',
-          type: 'item',
-          url: '/maintenance/coming-soon',
-          target: true
-        },
-        {
-          id: 'coming-soon-2',
-          title: 'coming-soon-2',
-          type: 'item',
-          url: '/maintenance/coming-soon-2',
-          target: true
-        },
-        {
-          id: 'under-construction',
-          title: 'under-construction',
-          type: 'item',
-          url: '/maintenance/under-construction',
-          target: true
-        }
       ]
     },
-    {
-      id: 'contact-us',
-      title: 'contact-us',
-      type: 'item',
-      url: '/contact-us',
-      icon: icons.contactus,
-      target: true
-    }
   ]
 };
 
--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -2,21 +2,10 @@ import { lazy } from 'react';
 
 // project-imports
 import Loadable from 'components/Loadable';
-import { SimpleLayoutType } from 'config';
 import DashboardLayout from 'layout/Dashboard';
-import PagesLayout from 'layout/Pages';
-import SimpleLayout from 'layout/Simple';
-
-// pages routing
-const MaintenanceError = Loadable(lazy(() => import('pages/maintenance/error/404')));
-const MaintenanceError500 = Loadable(lazy(() => import('pages/maintenance/error/500')));
-const MaintenanceUnderConstruction = Loadable(lazy(() => import('pages/maintenance/under-construction/under-construction')));
-const MaintenanceUnderConstruction2 = Loadable(lazy(() => import('pages/maintenance/under-construction/under-construction2')));
-const MaintenanceComingSoon = Loadable(lazy(() => import('pages/maintenance/coming-soon/coming-soon')));
-const MaintenanceComingSoon2 = Loadable(lazy(() => import('pages/maintenance/coming-soon/coming-soon2')));
+import Clients from '../pages/clients';
 
 // render - sample page
-const SamplePage = Loadable(lazy(() => import('pages/extra-pages/sample-page')));
 const Dashboard = Loadable(lazy(() => import('pages/dashboard')));
 
 const ContactUS = Loadable(lazy(() => import('pages/contact-us')));
@@ -38,45 +27,14 @@ const MainRoutes = {
     },
     {
       path: '/',
-      element: <SimpleLayout layout={SimpleLayoutType.SIMPLE} />,
+      element: <DashboardLayout />,
       children: [
         {
-          path: 'contact-us',
-          element: <ContactUS />
-        }
+          path: 'dashboard/clients',
+          element: <Clients />
+        },
       ]
     },
-    {
-      path: '/maintenance',
-      element: <PagesLayout />,
-      children: [
-        {
-          path: '404',
-          element: <MaintenanceError />
-        },
-        {
-          path: '500',
-          element: <MaintenanceError500 />
-        },
-        {
-          path: 'under-construction',
-          element: <MaintenanceUnderConstruction />
-        },
-        {
-          path: 'under-construction2',
-          element: <MaintenanceUnderConstruction2 />
-        },
-        {
-          path: 'coming-soon',
-          element: <MaintenanceComingSoon />
-        },
-        {
-          path: 'coming-soon-2',
-          element: <MaintenanceComingSoon2 />
-        }
-      ]
-    },
-    { path: '*', element: <MaintenanceError /> }
   ]
 };
 
